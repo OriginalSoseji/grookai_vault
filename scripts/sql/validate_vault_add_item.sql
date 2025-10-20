244774ccd1fb57a1c65fa8999f33549fcb2adc2b
Cesar Cabral <ccabrl@gmail.com>
2025-10-19 21:45:49 -0600

    docs(db): add validation script and README pointer

---
+++ scripts/sql/validate_vault_add_item.sql
@@ -0,0 +1,8 @@
+﻿-- Validate public.vault_add_item without persisting changes
+begin;
+select public.vault_add_item(
+  ''00000000-0000-0000-0000-000000000000''::uuid,
+  ''00000000-0000-0000-0000-000000000000''::uuid,
+  ''NM''
+) as new_id;
+rollback;
